ebf4a5702b5cfd855b7fc8fd73093afbd6f1f102
chrisworld <lbsnike@gmail.com>
2024-07-23 09:26:11 +0900

    함수 변경

--- DemoLoop.py
+++ DemoLoop.py
@@ -7,3 +7,13 @@ while value > 0:
 lst = ["apple", 100, 3.14]
 for item in lst:
     print(item)
+
+print(list(range(1,11)))
+print(list(range(2000,2025)))
+print(list(range(1,32)))
+
+lst = list(range(1,11))
+print([i**2 for i in lst if i > 5])
+tp = ("apple", "orange")
+print([len(i) for i in tp])
+
