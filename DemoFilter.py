f19dcd02c416f0dfba211080d6d8f07b404fcaba
chrisworld <lbsnike@gmail.com>
2024-07-23 09:38:53 +0900

    필터

---
+++ DemoFilter.py
@@ -0,0 +1,16 @@
+lst  = [10,25,30]
+iterL = filter(None, lst)
+for item in iterL:
+    print("item:{0}".format(item))
+
+
+def getBiggerThan20(i):
+    return i > 20
+
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print("item:{0}".format(item))
+
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print("item:{0}".format(item))
